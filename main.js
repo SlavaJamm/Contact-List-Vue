e63a556ec2c322dd7f367b69a27291d60b897ce1
slavajamm <kvaracheliyaRAM@gmail.com>
2018-09-05 15:18:26 +0300

    vers 1.0.0

---
+++ main.js
@@ -0,0 +1,527 @@
+new Vue ({
+    el: '#app',
+    data: {
+        characters: ['angry', 'sad', 'happy'],
+        people: [
+            {
+              "_id": "5b8fafd11b635861422ea0b9",
+              "index": 0,
+              "guid": "9d47df17-5a26-4f79-995a-c131bd976322",
+              "isActive": false,
+              "balance": "$3,460.85",
+              "picture": "https://g.cz/sites/default/files/field/image/2017/636199545744532358478577021_thinking_man_png11595.png",
+              "age": 35,
+              "eyeColor": "blue",
+              "name": {
+                "first": "Rivers",
+                "last": "Madden"
+              },
+              "company": "SKINSERVE",
+              "email": "rivers.madden@skinserve.name",
+              "phone": "+1 (956) 563-3931",
+              "address": "226 Brevoort Place, Wollochet, Michigan, 4224",
+              "about": "Deserunt aute magna officia ea velit et incididunt. Proident ex fugiat mollit exercitation duis duis minim amet proident commodo magna minim pariatur. Magna commodo proident occaecat labore nostrud cillum excepteur sunt Lorem ea ex proident voluptate. Exercitation enim culpa et dolore nostrud adipisicing consequat. Amet magna incididunt incididunt incididunt labore anim reprehenderit. Excepteur do ipsum aliquip anim sint consequat.",
+              "registered": "Sunday, May 4, 2014 4:07 AM",
+              "latitude": "2.316105",
+              "longitude": "92.291108",
+              "tags": [
+                "dolore",
+                "irure",
+                "dolore",
+                "labore",
+                "laboris"
+              ],
+              "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+              ],
+              "friends": [
+                {
+                  "id": 0,
+                  "name": "Julia Robles"
+                },
+                {
+                  "id": 1,
+                  "name": "House Baird"
+                },
+                {
+                  "id": 2,
+                  "name": "Tricia Blake"
+                }
+              ],
+              "greeting": "Hello, Rivers! You have 7 unread messages.",
+              "favoriteFruit": "apple"
+            },
+            {
+              "_id": "5b8fafd1be024db49fc853bc",
+              "index": 1,
+              "guid": "1ba3567a-196c-4ea5-86d5-e389bc557759",
+              "isActive": true,
+              "balance": "$2,755.05",
+              "picture": "https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&h=350",
+              "age": 40,
+              "eyeColor": "green",
+              "name": {
+                "first": "Sheryl",
+                "last": "Chambers"
+              },
+              "company": "DOGSPA",
+              "email": "sheryl.chambers@dogspa.com",
+              "phone": "+1 (820) 431-2603",
+              "address": "521 Mill Lane, Skyland, Nevada, 5588",
+              "about": "Eu excepteur reprehenderit id officia id dolore laborum ad aute et laborum eiusmod incididunt cupidatat. Quis enim cillum dolore Lorem ipsum. Ea non eiusmod commodo cillum do ipsum consectetur proident commodo veniam ullamco fugiat voluptate.",
+              "registered": "Saturday, July 2, 2016 5:05 AM",
+              "latitude": "63.84043",
+              "longitude": "53.123174",
+              "tags": [
+                "amet",
+                "excepteur",
+                "ipsum",
+                "sit",
+                "consectetur"
+              ],
+              "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+              ],
+              "friends": [
+                {
+                  "id": 0,
+                  "name": "Opal Saunders"
+                },
+                {
+                  "id": 1,
+                  "name": "Mckenzie Lawrence"
+                },
+                {
+                  "id": 2,
+                  "name": "Townsend Carey"
+                }
+              ],
+              "greeting": "Hello, Sheryl! You have 6 unread messages.",
+              "favoriteFruit": "strawberry"
+            },
+            {
+              "_id": "5b8fafd14f81382df3c1fc2c",
+              "index": 2,
+              "guid": "f68667a6-4986-4ca8-99b8-8cd9b20b5f83",
+              "isActive": false,
+              "balance": "$1,924.92",
+              "picture": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJb6iQXgVUTVxB_oe_D_FwMSW853gsw_LoOLf6V6tMJV-tto58",
+              "age": 35,
+              "eyeColor": "brown",
+              "name": {
+                "first": "Jessie",
+                "last": "Vasquez"
+              },
+              "company": "HATOLOGY",
+              "email": "jessie.vasquez@hatology.org",
+              "phone": "+1 (979) 577-2605",
+              "address": "195 Bragg Street, Heil, Louisiana, 5063",
+              "about": "Sunt cillum mollit qui aliqua ex commodo voluptate ex dolor nulla sunt dolor. Nisi excepteur consectetur consequat ad ex quis. Cupidatat veniam adipisicing adipisicing labore velit irure. Tempor mollit incididunt ut pariatur culpa laborum commodo adipisicing Lorem tempor qui.",
+              "registered": "Monday, February 12, 2018 7:16 AM",
+              "latitude": "-16.707087",
+              "longitude": "-116.922525",
+              "tags": [
+                "ipsum",
+                "sit",
+                "adipisicing",
+                "occaecat",
+                "esse"
+              ],
+              "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+              ],
+              "friends": [
+                {
+                  "id": 0,
+                  "name": "Buckley Tran"
+                },
+                {
+                  "id": 1,
+                  "name": "Araceli Serrano"
+                },
+                {
+                  "id": 2,
+                  "name": "Savage Harrington"
+                }
+              ],
+              "greeting": "Hello, Jessie! You have 5 unread messages.",
+              "favoriteFruit": "strawberry"
+            },
+            {
+              "_id": "5b8fafd168b855b9f95e6db1",
+              "index": 3,
+              "guid": "ba2c5167-389b-4c93-8343-e25bb3806210",
+              "isActive": true,
+              "balance": "$2,055.89",
+              "picture": "http://www.jakubhrab.com/wp/wp-content/uploads/2016/04/WEB_DSC_5403-Edit.jpg",
+              "age": 39,
+              "eyeColor": "green",
+              "name": {
+                "first": "Chase",
+                "last": "Beck"
+              },
+              "company": "MIRACULA",
+              "email": "chase.beck@miracula.us",
+              "phone": "+1 (894) 425-2352",
+              "address": "948 Downing Street, Farmington, Vermont, 6433",
+              "about": "Labore sit magna fugiat do. Nostrud ipsum eu quis esse adipisicing ea. Aliquip reprehenderit cillum qui sunt ex consectetur velit elit officia exercitation. Occaecat ullamco quis amet do est do ut. Incididunt irure esse eiusmod anim pariatur.",
+              "registered": "Friday, May 16, 2014 5:03 PM",
+              "latitude": "-10.565354",
+              "longitude": "109.876626",
+              "tags": [
+                "qui",
+                "duis",
+                "consequat",
+                "voluptate",
+                "cupidatat"
+              ],
+              "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+              ],
+              "friends": [
+                {
+                  "id": 0,
+                  "name": "Diann Doyle"
+                },
+                {
+                  "id": 1,
+                  "name": "Cora Sims"
+                },
+                {
+                  "id": 2,
+                  "name": "Thelma Myers"
+                }
+              ],
+              "greeting": "Hello, Chase! You have 6 unread messages.",
+              "favoriteFruit": "apple"
+            },
+            {
+              "_id": "5b8fafd17817d741da216aaf",
+              "index": 4,
+              "guid": "8299f6ea-6673-468d-856a-3f9a9862ed86",
+              "isActive": false,
+              "balance": "$3,622.73",
+              "picture": "https://www.loyatic.eu/wp-content/uploads/2017/11/iStock_000020004182Medium1.jpg",
+              "age": 32,
+              "eyeColor": "brown",
+              "name": {
+                "first": "Melton",
+                "last": "Gilmore"
+              },
+              "company": "BILLMED",
+              "email": "melton.gilmore@billmed.net",
+              "phone": "+1 (845) 456-3547",
+              "address": "718 Franklin Avenue, Lowgap, Nebraska, 4176",
+              "about": "Anim in magna ex elit nisi exercitation excepteur occaecat magna aliqua. Consectetur culpa ut consequat sunt reprehenderit. Cupidatat ad dolor ad non veniam culpa ullamco.",
+              "registered": "Wednesday, June 8, 2016 8:06 PM",
+              "latitude": "-73.414262",
+              "longitude": "-45.533259",
+              "tags": [
+                "enim",
+                "irure",
+                "anim",
+                "culpa",
+                "nulla"
+              ],
+              "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+              ],
+              "friends": [
+                {
+                  "id": 0,
+                  "name": "Lopez Gordon"
+                },
+                {
+                  "id": 1,
+                  "name": "Lucinda Barrett"
+                },
+                {
+                  "id": 2,
+                  "name": "Valdez Strong"
+                }
+              ],
+              "greeting": "Hello, Melton! You have 9 unread messages.",
+              "favoriteFruit": "strawberry"
+            },
+            {
+              "_id": "5b8fafd159abb6c6155b1952",
+              "index": 5,
+              "guid": "72546d43-930d-4cd5-b90d-5bcbaf223fa9",
+              "isActive": false,
+              "balance": "$3,814.29",
+              "picture": "http://www.wholemom.com/wp-content/uploads/2018/08/9-Man-Of-The-Year-Prize-Pack-Sweepstakes.jpg",
+              "age": 38,
+              "eyeColor": "brown",
+              "name": {
+                "first": "Jeannie",
+                "last": "Benson"
+              },
+              "company": "ESCHOIR",
+              "email": "jeannie.benson@eschoir.me",
+              "phone": "+1 (965) 552-2918",
+              "address": "407 Norman Avenue, Bawcomville, Montana, 8355",
+              "about": "Proident laboris deserunt non nostrud do ut enim amet. Magna ea voluptate ex ex est sint laborum esse. Velit mollit cillum excepteur amet. Ullamco ad esse adipisicing proident Lorem consectetur non eu exercitation officia sunt aliqua amet ullamco. Eu fugiat in reprehenderit reprehenderit. Ad quis aliquip laboris qui elit mollit sint cupidatat commodo minim consectetur occaecat sunt. Esse deserunt nostrud sunt sint incididunt nisi in.",
+              "registered": "Saturday, January 11, 2014 7:12 AM",
+              "latitude": "-85.796841",
+              "longitude": "134.791596",
+              "tags": [
+                "esse",
+                "eiusmod",
+                "cupidatat",
+                "ullamco",
+                "dolor"
+              ],
+              "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+              ],
+              "friends": [
+                {
+                  "id": 0,
+                  "name": "Rita Banks"
+                },
+                {
+                  "id": 1,
+                  "name": "Bessie Gonzalez"
+                },
+                {
+                  "id": 2,
+                  "name": "Owen Chan"
+                }
+              ],
+              "greeting": "Hello, Jeannie! You have 6 unread messages.",
+              "favoriteFruit": "strawberry"
+            },
+            {
+              "_id": "5b8fafd12b54346255eeb37f",
+              "index": 6,
+              "guid": "1f75f533-88cf-44e6-a97f-1f7a0f3f75ba",
+              "isActive": false,
+              "balance": "$1,903.87",
+              "picture": "https://cdn.cnn.com/cnnnext/dam/assets/170726110140-cnn-vasco-cotovio-profile-full-169.jpg",
+              "age": 38,
+              "eyeColor": "green",
+              "name": {
+                "first": "Yvette",
+                "last": "Wynn"
+              },
+              "company": "FLEETMIX",
+              "email": "yvette.wynn@fleetmix.tv",
+              "phone": "+1 (932) 477-2493",
+              "address": "932 Hooper Street, Sparkill, Arizona, 601",
+              "about": "Nisi do consequat sit cupidatat amet commodo. Cillum elit cillum ex anim irure excepteur dolore non consequat. Labore quis laborum proident deserunt irure ea in. Incididunt et amet quis esse do duis. Reprehenderit velit duis dolor mollit sunt fugiat occaecat ad excepteur officia. Sit et voluptate ut laborum exercitation. Veniam ullamco ut nulla excepteur veniam in minim enim ipsum.",
+              "registered": "Wednesday, July 13, 2016 5:30 PM",
+              "latitude": "19.02166",
+              "longitude": "48.264426",
+              "tags": [
+                "consectetur",
+                "in",
+                "et",
+                "amet",
+                "qui"
+              ],
+              "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+              ],
+              "friends": [
+                {
+                  "id": 0,
+                  "name": "Calderon Fowler"
+                },
+                {
+                  "id": 1,
+                  "name": "Reilly Wilson"
+                },
+                {
+                  "id": 2,
+                  "name": "Branch Galloway"
+                }
+              ],
+              "greeting": "Hello, Yvette! You have 5 unread messages.",
+              "favoriteFruit": "strawberry"
+            },
+            {
+              "_id": "5b8fafd1f7ddd8f7f63862ef",
+              "index": 7,
+              "guid": "5cba930e-e99f-4792-bece-873b5ee87aea",
+              "isActive": false,
+              "balance": "$3,029.76",
+              "picture": "https://cdn-01.independent.ie/incoming/article37204979.ece/6f814/AUTOCROP/w620/police002.jpg",
+              "age": 22,
+              "eyeColor": "brown",
+              "name": {
+                "first": "Newman",
+                "last": "Mcintyre"
+              },
+              "company": "EXOTERIC",
+              "email": "newman.mcintyre@exoteric.info",
+              "phone": "+1 (863) 477-3763",
+              "address": "661 Dorset Street, Yogaville, Marshall Islands, 816",
+              "about": "Amet adipisicing aliqua fugiat eu voluptate et officia labore commodo pariatur quis. Cupidatat culpa laboris ut sint ipsum cupidatat eiusmod consequat ut voluptate mollit minim aliquip Lorem. In voluptate aliqua Lorem consectetur magna laboris.",
+              "registered": "Monday, August 28, 2017 11:02 PM",
+              "latitude": "-37.295955",
+              "longitude": "71.572952",
+              "tags": [
+                "elit",
+                "officia",
+                "aliqua",
+                "occaecat",
+                "ut"
+              ],
+              "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+              ],
+              "friends": [
+                {
+                  "id": 0,
+                  "name": "Carla Blankenship"
+                },
+                {
+                  "id": 1,
+                  "name": "Hinton Garrett"
+                },
+                {
+                  "id": 2,
+                  "name": "Spears Singleton"
+                }
+              ],
+              "greeting": "Hello, Newman! You have 6 unread messages.",
+              "favoriteFruit": "strawberry"
+            },
+            {
+              "_id": "5b8fafd11a368aba517b8b8f",
+              "index": 8,
+              "guid": "43a8a377-6cd2-4245-83a8-7d6a981546d2",
+              "isActive": true,
+              "balance": "$3,186.99",
+              "picture": "http://www.pngmart.com/files/3/Man-PNG-Pic.png",
+              "age": 36,
+              "eyeColor": "brown",
+              "name": {
+                "first": "Margie",
+                "last": "Carney"
+              },
+              "company": "MAXIMIND",
+              "email": "margie.carney@maximind.biz",
+              "phone": "+1 (813) 416-2958",
+              "address": "869 Henderson Walk, Shepardsville, Alaska, 8987",
+              "about": "Nisi culpa magna laboris nulla nulla nostrud ad tempor. Cupidatat elit velit fugiat nulla occaecat nulla sit aliquip ut amet pariatur. Tempor voluptate ex dolor et laborum aliquip deserunt ut nisi ipsum cupidatat ea. Nisi cillum dolore do ipsum aliqua labore qui.",
+              "registered": "Monday, June 2, 2014 12:33 AM",
+              "latitude": "-80.455001",
+              "longitude": "161.312724",
+              "tags": [
+                "fugiat",
+                "sint",
+                "exercitation",
+                "velit",
+                "adipisicing"
+              ],
+              "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+              ],
+              "friends": [
+                {
+                  "id": 0,
+                  "name": "Rice Ochoa"
+                },
+                {
+                  "id": 1,
+                  "name": "Toni Weiss"
+                },
+                {
+                  "id": 2,
+                  "name": "Gretchen Kane"
+                }
+              ],
+              "greeting": "Hello, Margie! You have 10 unread messages.",
+              "favoriteFruit": "banana"
+            }
+        ]
+    },
+    methods: {
+
+    },
+    computed: {
+        
+    }
+})
